2928e5d0bea8601a6d59b35ec62be15659e781f3
Shobhit Asthana <114984222+shobhitasthana1@users.noreply.github.com>
2026-06-09 22:43:16 +0530

    A JavaScript DOM project that generates randomly positioned boxes with unique RGB colors on every button click, showcasing dynamic element creation and styling.

--- 11-DOM/Create-Element-Using-Dom/Project-02-Random-Box-Generator/script.js
+++ 11-DOM/Create-Element-Using-Dom/Project-02-Random-Box-Generator/script.js
@@ -12,13 +12,19 @@ btn.addEventListener('click', function () {
     var topPos = Math.random() * 80;
     var leftPos = Math.random() * 80;
 
+      var c1 = Math.random() * 256
+    var c2 = Math.random() * 256
+    var c3 = Math.random() * 256
+
     box.style.width = '100px';
     box.style.height = '100px';
-    box.style.backgroundColor = 'red';
+    box.style.backgroundColor = `rgb(${c1}, ${c2}, ${c3})`;
+
     box.style.position = 'fixed';
     box.style.top = `${topPos}%`;
     box.style.left = `${leftPos}%`;
     box.style.zIndex = '2';
+
     document.querySelector('main').append(box)
     console.log(box)
 })
