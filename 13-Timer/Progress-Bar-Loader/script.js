a39cd9b458d0b2ad5c64c96d2e483f6c4673bc95
Shobhit Asthana <114984222+shobhitasthana1@users.noreply.github.com>
2026-06-23 01:31:47 +0530

    Added Project : Progress Bar Loader

---
+++ 13-Timer/Progress-Bar-Loader/script.js
@@ -0,0 +1,22 @@
+var btn = document.querySelector('button');
+var h1 = document.querySelector('h1');
+var inner = document.querySelector('.inner');
+
+var grow = 0;
+
+btn.addEventListener("click", function () {
+
+    var interval = setInterval(() => {
+
+        h1.innerHTML = grow + "%";
+        inner.style.width = grow + "%";
+
+        grow++;
+
+        if (grow > 100) {
+            clearInterval(interval);
+        }
+
+    }, 100);
+
+});
--- 13-Timer/setInterval/script.js
+++ 13-Timer/setInterval/script.js
@@ -1,10 +1,21 @@
-var btn = document.querySelector("button");
+var btn1 = document.querySelector("#btn1");
+var btn2 = document.querySelector("#btn2");
+var int;
 
-btn.addEventListener("click", function(){
-    setInterval(function(){
+btn1.addEventListener("click", function(){
+ 
+    var count = 0;
+
+   int = setInterval(function(){
         var h1 = document.createElement('h1');
-        h1.innerHTML = "Hello"
+        h1.innerHTML = count;
         document.body.append(h1)
-        console.log(h1)
+        count++
     },1000)
+})
+
+btn2.addEventListener("click", function(){
+    setTimeout(() => {
+        clearInterval(int);
+    }, 10);
 })
