4e0ea87b8b3e8d18429ff5eb0b9542a3051082bf
Shobhit Asthana <114984222+shobhitasthana1@users.noreply.github.com>
2026-07-02 16:08:30 +0530

    ADDED : OOPS Section4/Question1

---
+++ 14-oop/assignments/section4/question1/script.js
@@ -0,0 +1,21 @@
+// Constructor Functions and Prototypes
+// 	10.	Create a User constructor function (do not use class syntax).
+// 	11.	Add a login method in two ways:
+// First, inside the constructor
+// Then, move the method to the prototype
+// 	12.	Create two User objects and compare their login methods using equality.
+// Explain why the result is true or false.
+
+function User(){
+    this.name = "Shobhit";
+    this.login = function(){
+       console.log("Logged In")
+    }
+}
+
+User.prototype.loggedin = function(){
+    console.log("Logged In")
+}
+
+user1 = new User;
+console.log(user1)
