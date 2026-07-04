a483d875d6bd1f28e4ce514f087f439d5443efaf
Shobhit Asthana <114984222+shobhitasthana1@users.noreply.github.com>
2026-07-05 00:23:26 +0530

    Another way to import all the named exports at once is by using the following syntax:

--- 17-import-&-Export/script.js
+++ 17-import-&-Export/script.js
@@ -6,7 +6,10 @@ import { username2 } from "./app.js";
 import { username3 } from "./app.js";
 import { username4 } from "./app.js";
 
-console.log(username1);
-console.log(username2);
-console.log(username3);
-console.log(username4);
+// console.log(username1);
+// console.log(username2);
+// console.log(username3);
+// console.log(username4);
+
+//Another way to import all the named exports at once is by using the following syntax:
+console.log(username1, username2, username3, username4);
