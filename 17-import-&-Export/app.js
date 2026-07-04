e143f43e08e25b8ae8cc29b2fc0464acdd540a95
Shobhit Asthana <114984222+shobhitasthana1@users.noreply.github.com>
2026-07-05 00:21:52 +0530

    Added Multiple Named Export

--- 17-import-&-Export/app.js
+++ 17-import-&-Export/app.js
@@ -1,6 +1,9 @@
 const user = "Shobhit";
 
-export const username = "Shobhit ASthana Ji"; // Named Export
+export const username1 = "Shobhit ASthana Ji"; // Named Export
+export const username2 = "Anshika Gupta"; // Named Export
+export const username3 = "Anshini Yadva"; // Named Export
+export const username4 = "Abhay raj"; // Named Export
 
 export default user; // Default Export
 
--- 17-import-&-Export/script.js
+++ 17-import-&-Export/script.js
@@ -1,5 +1,12 @@
 import user from "./app.js";
 console.log(user);
 
-import { username } from "./app.js";
-console.log(username);
+import { username1 } from "./app.js";
+import { username2 } from "./app.js";
+import { username3 } from "./app.js";
+import { username4 } from "./app.js";
+
+console.log(username1);
+console.log(username2);
+console.log(username3);
+console.log(username4);
