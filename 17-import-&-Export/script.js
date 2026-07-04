c62b9f306449143fc234861a3993ae9cf4ad5aac
Shobhit Asthana <114984222+shobhitasthana1@users.noreply.github.com>
2026-07-05 00:25:46 +0530

    //Another way to import all the named exports at once

--- 17-import-&-Export/script.js
+++ 17-import-&-Export/script.js
@@ -1,10 +1,10 @@
 import user from "./app.js";
 console.log(user);
 
-import { username1 } from "./app.js";
-import { username2 } from "./app.js";
-import { username3 } from "./app.js";
-import { username4 } from "./app.js";
+// import { username1 } from "./app.js";
+// import { username2 } from "./app.js";
+// import { username3 } from "./app.js";
+// import { username4 } from "./app.js";
 
 // console.log(username1);
 // console.log(username2);
@@ -12,4 +12,5 @@ import { username4 } from "./app.js";
 // console.log(username4);
 
 //Another way to import all the named exports at once is by using the following syntax:
+import {username1,username2,username3,username4} from "./app.js";
 console.log(username1, username2, username3, username4);
